36d8ab39adef1eed7aeed701d0db49047d963989
Shilpa <eclectic89@hotmail.com>
2024-08-07 13:16:53 +0100

    fixed TIcket 4

--- health.test.js
+++ health.test.js
@@ -1,14 +1,16 @@
 import {test, expect} from "vitest"
-import express from "/app.js"
+import app from "../app.js"
 import request from "supertest"
-import { parse } from "dotenv"
+
+
 //Make a test case for the api
 
-describe("GET /api/health", function () {
-    it("responds with health status", async function () {
-      const response = await request(app).get("/api/health").set("Accept", "application/json");
-      expect(response.headers["content-type"]).toMatch(/json/);
-      expect(response.status).toEqual(200);
-      expect(response.body.status).toEqual("healthy");
-    });
+test("GET /api/health works", async () => {
+  const response = await request(app).get("/api/health");
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual({
+    success: true,
+    payload: "API is running correctly"
   });
+
+});
